8d0ec7fd7bb384a550ba4faa9b7e90d34dfade5f
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-10-14 14:09:07 +0200

    Start testing the queue

--- tests/QueueTest.hs
+++ tests/QueueTest.hs
@@ -2,10 +2,13 @@ module QueueTest(
   queueTests
 ) where
 
-import           Test.HUnit
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+
+import SetQueue
 
 queueTests = [
-    case_testOrderPassable
+    testProperty "list to queue to list" prop_sort1
   ]
 
-case_testOrderPassable = True @? "Not detecting an unoccupied tile" 
+prop_sort1 :: [Int] -> Bool
+prop_sort1 xs = (queueToList . listToQueue) xs == xs
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,11 +1,7 @@
 import           Test.Framework                 (defaultMain, testGroup)
-import           Test.Framework.Providers.HUnit (testCase)
 
 import           QueueTest
 
-tests = [
-    testGroup "Queue" $ zipWith (testCase . show) [1 ..] queueTests
+main = defaultMain [
+    testGroup "Queue" queueTests
   ]
-
---------------------------------------------------
-main = defaultMain tests
